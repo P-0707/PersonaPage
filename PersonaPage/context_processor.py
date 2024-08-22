e770614cd7776764fb52f37860dfcf5b6cb74da7
Pavani <pavani070700@gamil.com>
2024-08-22 16:39:22 +0530

    change to deployment settings

--- PersonaPage/context_processor.py
+++ PersonaPage/context_processor.py
@@ -1,14 +1,18 @@
 from django.shortcuts import get_object_or_404
 from pages.models import Certification, Project, UserProfile
+from django.contrib.auth.models import User
 
 def project_context(request):
-    if request.user.is_authenticated:
-        # Try to get the UserProfile; if it doesn't exist, handle it gracefully
-        user_profile = UserProfile.objects.filter(user=request.user).first()
-        resume_url = user_profile.resume.url if user_profile and user_profile.resume else None
-    else:
+    try:
+        if request.user.is_authenticated:
+            user_profile = UserProfile.objects.filter(user=request.user).first()
+        else:
+            default_user = User.objects.get(username='root')
+            user_profile = UserProfile.objects.filter(user=default_user).first()
+    except User.DoesNotExist:
         user_profile = None
-        resume_url = None
+
+    resume_url = user_profile.resume.url if user_profile and user_profile.resume else None
 
     context = {
         'me': user_profile,
@@ -17,3 +21,4 @@ def project_context(request):
         'resume_url': resume_url,
     }
     return context
+
